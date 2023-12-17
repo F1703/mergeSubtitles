51911cb855d50b5d29b8d13cb179472b6e587089
francos <franco170391@gmail.com>
2023-12-16 23:44:30 -0300

    fix

--- mergeSubtitle.sh
+++ mergeSubtitle.sh
@@ -2,22 +2,8 @@
 
 url="https://subtitletools.com/merge-subtitles-online"
 api="https://subtitletools.com/api/v1/file-group/result"
-urlDonwload="https://subtitletools.com/download-file-job"
-
-cookie="/tmp/cookie.txt" 
-
-# name=_token
-# name=subtitles
-# name=second-subtitle
-# name=mode value=nearestCue
-# name=baseSubtitleColor value=#ffff54
-# name=mergeSubtitleColor  value=#ffff54
-# name=shouldColorBaseSubtitle value=1
-# name=shouldColorMergeSubtitle value=0
-# name=glueOffset value=1000
-# name=simpleTopBottom value=0
-# name=nearestCueTopBottom value=0
-# name=nearestCueThreshold value=0
+urlDonwload="https://subtitletools.com/download-file-job" 
+cookie="/tmp/cookiesss.txt" 
 
 modoDeUso() { 
     echo "Modo de uso: $0 -b baseFile.srt -m mergeFile.srt -s outputFile.srt ";
@@ -60,29 +46,35 @@ fi
 file1=${b} 
 file2=${m}
 
-_token=$(curl -s -X GET $url -c $cookie  | grep _token  | grep -oP 'value="(.*?)"' | sed 's/value=//g' | sed 's/"//g')
- 
-id_code=$(curl -s -X POST  $url -b $cookie  \
+_token=$(curl -s -X GET $url -c $cookie | grep _token  | grep -oP 'value="(.*?)"' | sed 's/value=//g' | sed 's/"//g')
+if [ $(echo -n $_token | wc -c ) -lt 1  ]; then echo "Token no encontrado\nSaliendo..\n"; exit; fi 
+
+id_code=$(curl -s -X POST  $url -b $cookie -c $cookie  \
     -F "_token=$_token" \
     -F "subtitles=@$file1" \
     -F "second-subtitle=@$file2" \
     -F "mode=nearestCue" \
-    -F "baseSubtitleColor=#ffff54"  \
-    -F "mergeSubtitleColor=#ffff54"  \
-    -F "shouldColorBaseSubtitle=1"  \
-    -F "shouldColorMergeSubtitle=0"  \
-    -F "glueOffset=1000"  \
-    -F "simpleTopBottom=0"  \
-    -F "nearestCueTopBottom=0"  \
-    -F "nearestCueThreshold=1000"  \
-    -H "User-Agent:Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:105.0) Gecko/20100101 Firefox/105.0" \
+    -F "baseSubtitleColor=#ffff54" \
+    -F "mergeSubtitleColor=#ffff54" \
+    -F "shouldColorBaseSubtitle=1" \
+    -F "shouldColorMergeSubtitle=0" \
+    -F "simpleTopBottom=0" \
+    -F "nearestCueTopBottom=0" \
+    -F "nearestCueThreshold=1000" \
+    -F "glueVideoLength=00:00:00" \
+    -H "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0" \
+    -H 'Origin: https://subtitletools.com' -H 'Connection: keep-alive' \
+    -H 'Referer: https://subtitletools.com/merge-subtitles-online' \
+    -H 'TE: trailers' \
     | grep -oP 'href="(.*?)"' | sed 's/href="//g' | sed 's/"//g' | awk '{print $NF}' FS='/') 
 
-sleep 2
-code=$(curl -s -c $cookie -X GET "$api/$id_code" | grep -oP 'id":(.*?),' | sed 's/id"://g' | sed 's/,//g'  ) 
- 
-_token=$(curl -s  -c $cookie-X GET "$url/$id_code" -b $cookie -c $cookie | grep csrf-token  | grep -oP 'content="(.*?)"' | sed 's/content="//g' | sed 's/"//g')
+if [ $(echo -n $id_code | wc -c ) -lt 1  ]; then echo "Token no encontrado\nSaliendo..\n"; exit; fi 
 
-curl -s -b $cookie -X POST "$urlDonwload/$id_code/$code"  -b $cookie -c $cookie -F "_token=$_token" > ${file3}
- 
+code=$(curl -s -X GET "$url/$id_code" -b $cookie -c $cookie | grep wire:key | grep -oP '="(.*?)"' | head -1 | tr -d '="') 
+if [ $(echo -n $code | wc -c ) -lt 1  ]; then echo "Token no encontrado\nSaliendo..\n"; exit; fi 
 
+_token=$(curl -s -X GET "$url/$id_code" -b $cookie -c $cookie | grep _token  | grep -oP 'value="(.*?)"' | sed 's/value=//g' | sed 's/"//g' | head -1) 
+if [ $(echo -n $_token | wc -c ) -lt 1  ]; then echo "Token no encontrado\nSaliendo..\n"; exit; fi 
+
+curl  -s -X POST "$urlDonwload/$id_code/$code"  -b $cookie -c $cookie -F "_token=$_token" -F "_method=post"    > ${file3}
+ 
