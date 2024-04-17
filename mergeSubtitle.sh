1abc72a359fba19e5b01e111513a418891641d1e
FH1703 <franco170391@gmail.com>
2024-04-16 23:37:25 -0300

    boundary post

--- mergeSubtitle.sh
+++ mergeSubtitle.sh
@@ -4,6 +4,9 @@ url="https://subtitletools.com/merge-subtitles-online"
 api="https://subtitletools.com/api/v1/file-group/result"
 urlDonwload="https://subtitletools.com/download-file-job" 
 cookie="/tmp/cookiesss.txt" 
+mi_data="/tmp/mi_data.txt"
+
+rm $cookie 2>/dev/null
 
 modoDeUso() { 
     echo "Modo de uso: $0 -b baseFile.srt -m mergeFile.srt -s outputFile.srt ";
@@ -46,27 +49,90 @@ fi
 file1=${b} 
 file2=${m}
 
+# generar numero aleatorio 
+boundary=""
+for ((i=1; i<=28; i++)); do
+    boundary="${boundary}$((RANDOM % 10))"
+done
+  
 _token=$(curl -s -X GET $url -c $cookie | grep _token  | grep -oP 'value="(.*?)"' | sed 's/value=//g' | sed 's/"//g')
+
 if [ $(echo -n $_token | wc -c ) -lt 1  ]; then echo -e "[-] Token no encontrado\nSaliendo..\n"; exit; fi 
 
-id_code=$(curl -s -X POST  $url -b $cookie -c $cookie  \
-    -F "_token=$_token" \
-    -F "subtitles=@$file1" \
-    -F "second-subtitle=@$file2" \
-    -F "mode=nearestCue" \
-    -F "baseSubtitleColor=#ffff54" \
-    -F "mergeSubtitleColor=#ffff54" \
-    -F "shouldColorBaseSubtitle=1" \
-    -F "shouldColorMergeSubtitle=0" \
-    -F "simpleTopBottom=0" \
-    -F "nearestCueTopBottom=0" \
-    -F "nearestCueThreshold=1000" \
-    -F "glueVideoLength=00:00:00" \
+cat <<EOF > $mi_data
+-----------------------------$boundary
+Content-Disposition: form-data; name="_token"
+
+$_token
+-----------------------------$boundary
+Content-Disposition: form-data; name="subtitles"; filename="$file1"
+Content-Type: application/x-subrip
+
+$(cat $file1)
+-----------------------------$boundary
+Content-Disposition: form-data; name="second-subtitle"; filename="$file2"
+Content-Type: application/x-subrip
+
+$(cat $file2)
+-----------------------------$boundary
+Content-Disposition: form-data; name="mode"
+
+nearestCue
+-----------------------------$boundary
+Content-Disposition: form-data; name="nearestCueThreshold"
+
+1000
+-----------------------------$boundary
+Content-Disposition: form-data; name="nearestCueTopBottom"
+
+0
+-----------------------------$boundary
+Content-Disposition: form-data; name="nearestCueRemoveLineBreaksFromBase"
+
+0
+-----------------------------$boundary
+Content-Disposition: form-data; name="nearestCueRemoveLineBreaksFromMerge"
+
+0
+-----------------------------$boundary
+Content-Disposition: form-data; name="simpleTopBottom"
+
+0
+-----------------------------$boundary
+Content-Disposition: form-data; name="glueVideoLength"
+
+00:00:00
+-----------------------------$boundary
+Content-Disposition: form-data; name="shouldColorMergeSubtitle"
+
+0
+-----------------------------$boundary
+Content-Disposition: form-data; name="shouldColorMergeSubtitle"
+
+1
+-----------------------------$boundary
+Content-Disposition: form-data; name="mergeSubtitleColor"
+
+#ffff54
+-----------------------------$boundary
+Content-Disposition: form-data; name="shouldColorBaseSubtitle"
+
+0
+-----------------------------$boundary
+Content-Disposition: form-data; name="baseSubtitleColor"
+
+#ffff54
+-----------------------------$boundary--
+EOF
+
+id_code=$(curl -s -b $cookie -c $cookie  -X POST  $url   \
     -H "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0" \
     -H 'Origin: https://subtitletools.com' -H 'Connection: keep-alive' \
+    -H 'Upgrade-Insecure-Requests: 1' -H 'Sec-Fetch-Dest: document' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-Site: same-origin' \
     -H 'Referer: https://subtitletools.com/merge-subtitles-online' \
-    -H 'TE: trailers' \
-    | grep -oP 'href="(.*?)"' | sed 's/href="//g' | sed 's/"//g' | awk '{print $NF}' FS='/') 
+    -H "Content-Type: multipart/form-data; boundary=---------------------------$boundary" \
+    -H 'Sec-Fetch-User: ?1' -H 'TE: trailers' \
+    --data-binary "@$mi_data"  | grep -oP 'href="(.*?)"' | sed 's/href="//g' | sed 's/"//g' | awk '{print $NF}' FS='/')
 
 if [ $(echo -n $id_code | wc -c ) -lt 1  ]; then echo -e "[-] Token no encontrado\nSaliendo..\n"; exit; fi 
 
@@ -76,5 +142,6 @@ if [ $(echo -n $code | wc -c ) -lt 1  ]; then echo -e "[-] Token no encontrado\n
 _token=$(curl -s -X GET "$url/$id_code" -b $cookie -c $cookie | grep _token  | grep -oP 'value="(.*?)"' | sed 's/value=//g' | sed 's/"//g' | head -1) 
 if [ $(echo -n $_token | wc -c ) -lt 1  ]; then echo -e "[-] Token no encontrado\nSaliendo..\n"; exit; fi 
 
-curl  -s -X POST "$urlDonwload/$id_code/$code"  -b $cookie -c $cookie -F "_token=$_token" -F "_method=post"    > ${file3}
+curl  -s -X POST "$urlDonwload/$id_code/$code"  -b $cookie -c $cookie -F "_token=$_token" -F "_method=post"   > ${file3}
+ 
  
